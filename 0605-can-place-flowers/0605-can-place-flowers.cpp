6684deae5e3fe76ed43430e9c23965d1ac850447
Saurav Kumar <124197263+Saurav13kr@users.noreply.github.com>
2023-05-03 13:43:31 +0530

    Time: 19 ms (51.04%), Space: 20.3 MB (31.31%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -4,19 +4,21 @@ public:
         if (n == 0) {
             return true ; 
         }
+        
         for (int i = 0; i < flowerbed.size();  i++) {
             if ((i == 0 || flowerbed[i - 1]==0) && 
                 
                (flowerbed[i] == 0) && 
-               (i == flowerbed.size() - 1 || flowerbed[i + 1] == 0)) 
-            {
+               (i == flowerbed.size() - 1 || flowerbed[i + 1] == 0)) {
                 flowerbed[i] = 1 ;
                 n-- ;
-            } 
+            }
+            
             if (n == 0) {
                 return true ; 
             }
         }
+        
         return false ; 
     }
 };
