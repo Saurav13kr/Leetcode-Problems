58c0f1b13d22e7fcd88de5dd449fa08e96966604
Saurav Kumar <124197263+Saurav13kr@users.noreply.github.com>
2023-06-06 11:49:06 +0530

    Time: 4 ms (74.54%), Space: 9.2 MB (5.60%) - LeetHub

--- 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.cpp
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
-    bool canMakeArithmeticProgression(vector<int>& arr) {
-        if(arr.size()==1 ||arr.size()==2){
+    bool canMakeArithmeticProgression(vector<int> & arr) {
+        if(arr.size() == 1 || arr.size() == 2){
             return true;
-        }
-        else{
-            sort(arr.begin(),arr.end());
-            int change = arr[0]-arr[1];
+            
+        } else{
+            sort(arr.begin(), arr.end());
+            int change = arr[0] - arr[1];
+            
             for (size_t i = 0; i < arr.size()-1; i++) {
-                if(arr[i]-arr[i+1]!=change){
+                if(arr[i] - arr[i+1] != change) {
                     return false;
                 }
             }
+            
             return true;
         }
     }
