794cc328cd5b1fa154ae6bf660962b7027fd31af
Saurav Kumar <124197263+Saurav13kr@users.noreply.github.com>
2023-06-26 22:21:43 +0530

    Time: 92 ms (48.68%), Space: 28.4 MB (32.59%) - LeetHub

---
+++ 0436-find-right-interval/0436-find-right-interval.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+     vector<int> findRightInterval(vector<vector<int>>& intervals) {
+        vector<int> res;
+        map<int,int> m;
+         
+        for(int i=0;i<intervals.size();i++) {  
+            m[intervals[i][0]]=i;    
+        }
+         
+        for(int i=0;i<intervals.size();i++) {
+            auto it=m.lower_bound(intervals[i][1]);
+            
+            if(it==m.end())
+                res.push_back(-1);
+            else
+                res.push_back(it->second);
+        }
+         
+        return res;
+    }
+};
