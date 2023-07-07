6825ff858050240e25c8ad4ef5f36cddc6b00303
Saurav Kumar <124197263+Saurav13kr@users.noreply.github.com>
2023-07-07 23:28:31 +0530

    Time: 52 ms (17.39%), Space: 122.5 MB (9.76%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+string ans;
+          void say(int n,string str,int num){
+                if(n==num+1){
+                ans=str;
+                return ;
+                }
+              
+                if(n==1){
+                    str="1";
+                    say(n+1,str,num);
+                    return;
+                }
+              
+               string s;
+               int cnt=1;
+               char c=str[0];
+               stack<char> st;
+              
+               for(int i=0;i<str.size();i++){
+                if(!st.empty()&& st.top()==str[i]){
+                    st.push(str[i]);
+                }
+                   
+                else if(st.empty())
+                st.push(str[i]);
+                else{
+                    s = s+to_string(st.size())+st.top();
+                    while(!st.empty()) st.pop();
+                    st.push(str[i]);
+                }
+            }
+            if(!st.empty()){
+                s=s+to_string(st.size())+st.top();
+            }
+               
+                say(n+1,s,num);
+            }
+
+    string countAndSay(int n) {
+       say(1,"",n);
+        
+       return ans;
+    }
+};
