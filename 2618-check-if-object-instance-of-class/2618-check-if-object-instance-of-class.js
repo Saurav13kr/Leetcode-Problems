bd4fd0bf94a7b0a235e8d6288d9647a84fa1cc19
Saurav Kumar <124197263+Saurav13kr@users.noreply.github.com>
2023-08-02 21:32:14 +0530

    Time: 121 ms (10.95%), Space: 53.4 MB (6.31%) - LeetHub

---
+++ 2618-check-if-object-instance-of-class/2618-check-if-object-instance-of-class.js
@@ -0,0 +1,24 @@
+/**
+ * @param {any} obj
+ * @param {any} classFunction
+ * @return {boolean}
+ */
+
+    
+
+function check(obj) {
+    var ans = [], cur = obj.__proto__;
+    while (cur && cur.name !== "Object") {
+        ans.push(cur.constructor.name);
+        cur = cur.__proto__;
+    }
+    return ans;
+}
+
+function isObject(val) {
+    return !["Null", "Undefined"].includes(/\s(\w+)\]/.exec(Object.prototype.toString.call(val))[1])
+}
+
+function checkIfInstanceOf(x, y) {
+    return isObject(x) && isObject(y) && y.constructor.name == "Function" && check(x).includes(y.name)
+}
