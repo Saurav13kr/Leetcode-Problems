e155c1fb7265888600747c60b673d7044b00e479
Saurav Kumar <124197263+Saurav13kr@users.noreply.github.com>
2023-08-18 21:17:18 +0530

    Time: 104 ms (46.22%), Space: 39.3 MB (28.54%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.cpp
+++ 1615-maximal-network-rank/1615-maximal-network-rank.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
+    int total=0,maxrank=0;
+    unordered_map<int,unordered_set<int>> adj;
+    
     int maximalNetworkRank(int n, vector<vector<int>>& roads) {
-        std::vector<int> degrees(n, 0);
-        std::unordered_map<int, std::unordered_set<int>> directlyConnected;
-        for (const auto& road : roads) {
-            degrees[road[0]]++;
-            degrees[road[1]]++;
-            directlyConnected[road[0]].insert(road[1]);
-            directlyConnected[road[1]].insert(road[0]);
+        for(auto &road :roads){
+            int u=road[0];
+            int v=road[1];
+
+            adj[u].insert(v);
+            adj[v].insert(u);
         }
         
-        int result = 0;
-        for (int i = 0; i < n; ++i) {
-            for (int j = i + 1; j < n; ++j) {
-                if (directlyConnected[i].count(j)) {
-                    result = std::max(result, degrees[i] + degrees[j] - 1);
-                } else {
-                    result = std::max(result, degrees[i] + degrees[j]);
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++) {
+                int i_rank=adj[i].size();
+                int j_rank=adj[j].size();
+                total= i_rank +j_rank;
+                if(adj[i].find(j) != adj[i].end()){
+                    total-=1;
                 }
+                maxrank=max(maxrank,total);
             }
         }
-        return result;
+        return maxrank;
     }
 };
