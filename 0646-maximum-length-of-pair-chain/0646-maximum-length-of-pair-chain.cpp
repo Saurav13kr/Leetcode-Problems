a95ba7e94cda026e8105cff6d88e7b76d786b856
Saurav Kumar <124197263+Saurav13kr@users.noreply.github.com>
2023-08-26 10:05:03 +0530

    Time: 67 ms (82.85%), Space: 23.9 MB (38.59%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
@@ -1,16 +1,27 @@
 class Solution {
 public:
+    static bool cmp(pair<int,int> p1,pair<int,int>p2) {
+        return p1.second < p2.second;
+    }
     int findLongestChain(vector<vector<int>>& pairs) {
-        vector<int> dp(pairs.size(), 1);
-        sort(pairs.begin(), pairs.end());
-        for (int j = 0; j < pairs.size(); j++) {
-            for (int i = 0; i < j; i++) {
-                if (pairs[i][1] < pairs[j][0]) {
-                    dp[j] = 1 + dp[i];
-                }
-            }
+        vector<pair<int,int>> toStorePair;
+
+        for(int i=0;i<pairs.size();i++){
+            pair<int,int> p = make_pair(pairs[i][0],pairs[i][1]);
+            toStorePair.push_back(p);
         }
         
-        return *max_element(dp.begin(), dp.end());
+        sort(toStorePair.begin(),toStorePair.end(),cmp);
+        int ans = 1;
+        int ansMax = toStorePair[0].second;
+        
+        for(int i=0;i<toStorePair.size();i++){
+            if(ansMax < toStorePair[i].first) {
+                ans++;
+                ansMax = toStorePair[i].second;
+            }
+            cout<< toStorePair[i].first <<" "<< toStorePair[i].second<<" ";
+        }
+        return ans;
     }
 };
