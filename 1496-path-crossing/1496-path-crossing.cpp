fb012245d5097c1c2f2185fb0f637a06356b52ea
Saurav Kumar <124197263+Saurav13kr@users.noreply.github.com>
2023-12-23 20:15:08 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (64.64%) - LeetHub

--- 1496-path-crossing/1496-path-crossing.cpp
+++ 1496-path-crossing/1496-path-crossing.cpp
@@ -5,13 +5,15 @@ public:
         int y=0;
         unordered_set<int>s{0};
         int curr = x+(1e5)*y;
+        
         for(auto i:path){
             if(i=='N') y++;
             else if(i=='S') y--;
             else if(i=='E') x++;
             else x--;
             curr = x+(1e5)*y;
-            if(s.count(curr)) return true;
+            if(s.count(curr))
+                return true;
             s.insert(curr);
         }
         return false;
