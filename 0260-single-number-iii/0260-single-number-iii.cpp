35ea8c4e88505fb6d3dd746f4ee2ebb76c075922
Saurav Kumar <124197263+Saurav13kr@users.noreply.github.com>
2024-02-12 10:53:12 +0530

    Time: 11 ms (33.09%), Space: 13.3 MB (14.77%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        map<int,int> mp;
+        vector<int> ans;
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]++;
+        }
+        for(auto m:mp){
+            if(m.second==1){
+                ans.push_back(m.first);
+            }
+        }
+        return ans;
+    }
+};
