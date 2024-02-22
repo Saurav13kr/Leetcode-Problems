10b3e93133c9f7b033ddb4f16d1fc8c3b3ce5e37
Saurav Kumar <124197263+Saurav13kr@users.noreply.github.com>
2024-02-22 14:02:17 +0530

    Time: 127 ms (77.06%), Space: 64.3 MB (69.38%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
-    int findJudge(int n, vector<vector<int>>& trust) {        
-        vector<int> cnt1(n+1);  
-        vector<int> cnt2(n+1); 
-
-        for(vector<int>& t: trust){
-            cnt1[t[0]]++;
-            cnt2[t[1]]++;
+    int findJudge(int n, vector<vector<int>>& trust) {
+        vector<int>v(n + 1, 0); 
+        vector<int>v2(n + 1, 0);       
+        for(int i = 0; i < trust.size(); ++i){
+            v[trust[i][1]]++;
+            v2[trust[i][0]]++;
         }
-
-        for(int i=1; i<=n; i++){ 
-            if(cnt1[i] == 0 && cnt2[i] == n-1){ 
-                return i; 
+        for(int i = 1; i <= n; ++i){
+            if(v[i] == n - 1 && v2[i] == 0){
+               return i;
             }
         }
         return -1;
