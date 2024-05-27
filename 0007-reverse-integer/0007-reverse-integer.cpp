d9b7d637fea80ac67f5f390b059743dbe6ffd586
Saurav Kumar <124197263+Saurav13kr@users.noreply.github.com>
2024-05-27 10:56:14 +0530

    Update 0007-reverse-integer.cpp

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
-    int reverse(int x) {
+    int reverse(int x) 
+{
         int y=0;
         
         while(x) {
-            if(y>INT_MAX/10 || y<INT_MIN/10){
+            if(y>INT_MAX/10 || y<INT_MIN/10)
+            {
                 return 0;
             }
             else {
