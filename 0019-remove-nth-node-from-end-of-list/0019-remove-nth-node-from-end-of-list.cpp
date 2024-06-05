eece4536c4469e66cd6ce1caba0a363437c666df
Saurav Kumar <124197263+Saurav13kr@users.noreply.github.com>
2024-06-05 13:40:28 +0530

    Update 0019-remove-nth-node-from-end-of-list.cpp

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -1,13 +1,4 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+
 class Solution {
 public:
     //Function to calculate length of LL
@@ -25,7 +16,8 @@ public:
          return l;
      }
 
-    ListNode* removeNthFromEnd(ListNode* head, int n) {
+    ListNode* removeNthFromEnd(ListNode* head, int n) 
+{
         ListNode* temp=head;
         int len = length(head);
         int cnt = 1;
