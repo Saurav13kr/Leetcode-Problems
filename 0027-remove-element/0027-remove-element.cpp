ba50d967fe06129fe0a65925806a1322400a8292
Saurav Kumar <124197263+Saurav13kr@users.noreply.github.com>
2024-06-05 13:40:59 +0530

    Update 0027-remove-element.cpp

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
-    int removeElement(vector<int>& nums, int val) {
-        int j = 0;
-        
+    int removeElement(vector<int>& nums, int val) 
+{
+        int j = 0;      
         for(int i=0; i<nums.size(); i++) {
             if(nums[i] != val)
             {
