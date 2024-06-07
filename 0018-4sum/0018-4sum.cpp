3d119c17fcd4d297cc529385d97829bef823ad8f
Saurav Kumar <124197263+Saurav13kr@users.noreply.github.com>
2024-06-07 16:44:59 +0530

    Update 0018-4sum.cpp

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -5,7 +5,8 @@ public:
         sort(nums.begin(), nums.end());
         vector<vector<int>> output;
         
-        for(int i=0; i<n-3; i++){
+        for(int i=0; i<n-3; i++)
+        {
             for(int j=i+1; j<n-2; j++){
                 long long newTarget = (long long)target - (long long)nums[i] - (long long)nums[j];
                 int low = j+1, high = n-1;
@@ -16,8 +17,7 @@ public:
                     }
                     else if(nums[low] + nums[high] > newTarget){
                         high--;
-                    }
-                    else{
+                    } else{
                         output.push_back({nums[i], nums[j], nums[low], nums[high]});
                         int tempIndex1 = low, tempIndex2 = high;
                         while(low < high && nums[low] == nums[tempIndex1]) low++;
