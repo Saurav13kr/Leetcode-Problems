899dbb262d0d759c85fbc3f3209730ce6dfa5b48
Saurav Kumar <124197263+Saurav13kr@users.noreply.github.com>
2024-06-15 15:46:35 +0530

    Update 0086-partition-list.cpp

--- 0086-partition-list/0086-partition-list.cpp
+++ 0086-partition-list/0086-partition-list.cpp
@@ -1,13 +1,4 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+
 class Solution {
 public:
     
@@ -22,9 +13,7 @@ public:
                 small = small -> next;
                 head = head -> next;
                 small->next = NULL;
-            }
-            
-            else{
+            }else{
                 large->next = head;
                 large = large -> next;
                 head = head -> next;
