6d7288779bc932b7f200935c3d54539c4dfd161f
Saurav Kumar <124197263+Saurav13kr@users.noreply.github.com>
2024-06-26 11:26:06 +0530

    Update 0050-powx-n.cpp

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    double myPow(double x, int n) {
+    double myPow(double x, int n) 
+{
         double res = 1;
         while (n) {
             if (n % 2) res = n > 0 ? res * x : res / x;
