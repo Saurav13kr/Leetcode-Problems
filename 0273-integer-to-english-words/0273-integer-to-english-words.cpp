714fbfc54243b3345d575d5e9def0ecbff31e47d
Saurav Kumar <124197263+Saurav13kr@users.noreply.github.com>
2024-08-07 12:20:14 +0530

    Time: 4 ms (49.19%), Space: 12.1 MB (50.28%) - LeetHub

--- 0273-integer-to-english-words/0273-integer-to-english-words.cpp
+++ 0273-integer-to-english-words/0273-integer-to-english-words.cpp
@@ -1,28 +1,68 @@
 class Solution {
 public:
-    string ones[20] = {"", " One", " Two", " Three", " Four", " Five", " Six", " Seven", " Eight", " Nine", " Ten", " Eleven", " Twelve", " Thirteen", " Fourteen", " Fifteen", " Sixteen", " Seventeen", " Eighteen", " Nineteen"};
-    string tens[10] = {"", " Ten", " Twenty", " Thirty", " Forty", " Fifty", " Sixty", " Seventy", " Eighty", " Ninety"};
-    string thousands[4] = {"", " Thousand", " Million", " Billion"};
-    string number(int n) {
-        if(n < 20){
-            return ones[n];
+    vector<std::string> counts={"Thousand","Million","Billion"};
+    std::vector<std::string> ones = {" ","One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
+    std::vector<std::string> tens = {" ","Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+    std::vector<std::string> teens = {"Ten","Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+    
+    string convertinttostring(int nums){
+        string ans;
+        bool lastspaceremove=true;
+        cout<<nums<<" "<<nums/100<<" "<<endl;
+        if(nums>=100) 
+        {
+            cout<<"1"<<endl;
+            ans+=ones[nums/100]+" Hundred "; nums%=100;
         }
-        if(n < 100){
-            return tens[n/10] + number(n%10);
+        if(nums>=20) 
+        {
+            cout<<"2"<<endl;
+            ans+=tens[(nums/10)-1]+" ";
+            nums%=10;
+            
         }
-        if(n<1000){
-           return ones[n/100] + " Hundred" + number(n%100);
+        if(nums>=10 ) 
+        {
+            cout<<"3"<<endl;
+            cout<<nums/10<<endl;
+            nums%=10;
+             ans+=teens[nums%10]+" ";
+            
         }
-        
-        for (int i = 3; i >= 0; i--) {
-            if (n >= pow(1000, i)) {
-                return number(n / pow(1000, i)) + thousands[i] + number(n % (int)pow(1000, i));
-            }
+        else if(nums>0) {
+            cout<<"4"<<endl;
+            ans+=ones[nums]+" ";
+            nums%=10;
+            
         }
-        return "";
+        return ans;
     }
-    string numberToWords(int num){
-        if(num == 0)    return "Zero";
-        return number(num).substr(1);
+    string numberToWords(int num) {
+        if(num==0) return "Zero";
+        string ans="";
+        int i=0;
+        while(num>0){
+            int j=1,no=0;
+ 
+            int reverseno=num%1000;
+            num/=1000;
+            string tempstr="";
+            tempstr=convertinttostring(reverseno);
+            cout<<"ans "<<tempstr<<endl;
+            if(i>0 && !tempstr.empty())
+            {
+                tempstr+=counts[i-1]+" ";
+            }
+            ans=tempstr+ans;
+            i++;
+
+        }
+        size_t endpos = ans.find_last_not_of(" ");
+        if (std::string::npos != endpos) {
+            ans = ans.substr(0, endpos + 1);
+        }
+
+        
+        return ans;
     }
 };
