d378b8f7cd4199b3119aa9b22cf6ed582847a4be
Saurav Kumar <124197263+Saurav13kr@users.noreply.github.com>
2024-08-18 16:15:21 +0530

    Time: 3 ms (76.61%), Space: 9.7 MB (72.38%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.cpp
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -1,16 +1,34 @@
 class Solution {
 public:
     int nthUglyNumber(int n) {
-        if (n == 1) return 1;
-        vector<int> dp(n);
-        dp[0] = 1;
-        int i2 = 0, i3 = 0, i5 = 0;
+        if(n <= 0) return 0;
+        if(n == 1) return 1;
+
+        vector<int> uglyNums(n);
+        uglyNums[0] = 1;  // First ugly number is 1
+        int i2 = 0, i3 = 0, i5 = 0;  // Indices for 2, 3, 5
+        int next_multiple_of_2 = 2;
+        int next_multiple_of_3 = 3;
+        int next_multiple_of_5 = 5;
+
         for(int i = 1; i < n; i++) {
-            dp[i] = min({dp[i2] * 2, dp[i3] * 3, dp[i5] * 5});
-            if (dp[i] == dp[i2] * 2) i2++;
-            if (dp[i] == dp[i3] * 3) i3++;
-            if (dp[i] == dp[i5] * 5) i5++;
+            int next_ugly = min(next_multiple_of_2, min(next_multiple_of_3, next_multiple_of_5));
+            uglyNums[i] = next_ugly;
+
+            if(next_ugly == next_multiple_of_2) 
+            {
+                i2++;
+                next_multiple_of_2 = uglyNums[i2] * 2;
+            }
+            if(next_ugly == next_multiple_of_3) {
+                i3++;
+                next_multiple_of_3 = uglyNums[i3] * 3;
+            }
+            if(next_ugly == next_multiple_of_5) {
+                i5++;
+                next_multiple_of_5 = uglyNums[i5] * 5;
+            }
         }
-        return dp[n - 1];
+        return uglyNums[n-1];
     }
 };
