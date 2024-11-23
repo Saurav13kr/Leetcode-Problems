e8d45649d9083da85caae3f99641f7f70cfb8410
Saurav Kumar <124197263+Saurav13kr@users.noreply.github.com>
2024-11-23 18:11:14 +0530

    Time: 207 ms (26.45%), Space: 8.8 MB (48.41%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,33 +1,37 @@
 class Solution {
-    bool areVectorsEqual(vector<int> a, vector<int> b){
-        for(int i=0; i<26; i++){
-            if(a[i]!=b[i]) return false;
-        }
-        return true;
-    }
-    
 public:
+       bool isFreqSame(int freq1[], int freq2[]) {
+			for(int i=0; i<26; i++) {
+				if(freq1[i] != freq2[i]) {
+					return false;
+				}
+			}
+			return true;
+		}
     bool checkInclusion(string s1, string s2) {
-        if(s2.size()<s1.size()) return false;
-        vector<int> freqS1(26, 0);
-        
-        for(char c: s1) freqS1[c-'a']++;        
-        vector<int> freqS2(26, 0);
-        int i=0, j=0; 
-        
-        while(j<s2.size()){
-            freqS2[s2[j]-'a']++;     
-            if(j-i+1==s1.size()){
-                if(areVectorsEqual(freqS1, freqS2)) return true;
-            }
-            
-            if(j-i+1<s1.size()) j++;
-            else{
-                freqS2[s2[i]-'a']--;
-                i++;
-                j++;
-            }
-        }
-        return false;
+			int freq[26] = {0};
+			
+			for(int i=0; i<s1.length(); i++){
+				freq[s1[i] - 'a']++;
+			}
+			
+			int windSize = s1.length();
+			
+			for(int i=0; i<s2.length(); i++) 
+            {
+				int windIdx = 0, idx = i;
+                int windFreq[26] = {0};
+				
+				while(windIdx < windSize && idx < s2.length()) {
+					windFreq[s2[idx] - 'a']++;
+					windIdx++; idx++;
+				}
+				
+				if(isFreqSame(freq, windFreq)) {       
+					return true;
+				}
+			}
+			
+			return false;   
     }
 };
