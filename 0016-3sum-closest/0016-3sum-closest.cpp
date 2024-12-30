8542882d9e79dec98aaf31f321628f28ae273e69
Saurav Kumar <124197263+Saurav13kr@users.noreply.github.com>
2024-12-30 18:22:35 +0530

    Update 0016-3sum-closest.cpp

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -4,7 +4,8 @@ public:
         sort(nums.begin(), nums.end());
         int n = nums.size(), ans = nums[0] + nums[1] + nums[2];
         
-        for (int i = 0; i < n-2; ++i) {
+        for (int i = 0; i < n-2; ++i) 
+        {
             int l = i + 1, r = n - 1;
             while (l < r) {
                 int sum3 = nums[i] + nums[l] + nums[r];
