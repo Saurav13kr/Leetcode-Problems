59f9537223dba4449b189ed7101c9ecd1d005d07
Saurav Kumar <124197263+Saurav13kr@users.noreply.github.com>
2025-01-03 17:43:25 +0530

    Update 0011-container-with-most-water.cpp

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -4,8 +4,7 @@ public:
        int ans = 0, n = height.size();
        int i = 0, j = n-1;
         
-       while(i<j)
-       {
+       while(i<j) {
            if(height[i]>height[j]){
                ans = max(ans, (j-i)*height[j]);
                j--;
