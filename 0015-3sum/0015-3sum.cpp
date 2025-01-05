0e409ff413576658f74660cedba6a369550ec7d8
Saurav Kumar <124197263+Saurav13kr@users.noreply.github.com>
2025-01-05 15:34:00 +0530

    Update 0015-3sum.cpp

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -8,7 +8,8 @@ public:
         for(int i=0; i<n-2; i++){
             int low = i+1, high = n-1;
             while(low < high){
-                if(nums[i] + nums[low] + nums[high] < 0){
+                if(nums[i] + nums[low] + nums[high] < 0)
+                {
                     low++;
                 }
                 else if(nums[i] + nums[low] + nums[high] > 0){
