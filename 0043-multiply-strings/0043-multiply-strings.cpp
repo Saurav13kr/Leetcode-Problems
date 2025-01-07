d165474323542981ecbbba7729fde4e77fbeab77
Saurav Kumar <124197263+Saurav13kr@users.noreply.github.com>
2025-01-07 09:56:47 +0530

    Update 0043-multiply-strings.cpp

--- 0043-multiply-strings/0043-multiply-strings.cpp
+++ 0043-multiply-strings/0043-multiply-strings.cpp
@@ -5,7 +5,8 @@ public:
         vector<int> res(num1.size()+num2.size(), 0);
         
         for (int i = num1.size()-1; i >= 0; i--) {
-            for (int j = num2.size()-1; j >= 0; j--) {
+            for (int j = num2.size()-1; j >= 0; j--)
+                {
                 res[i + j + 1] += (num1[i]-'0') * (num2[j]-'0');
                 res[i + j] += res[i + j + 1] / 10;
                 res[i + j + 1] %= 10;
