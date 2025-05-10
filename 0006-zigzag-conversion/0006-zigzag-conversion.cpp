f130ea634bae2e1d45a405c104e2113e597eedb3
Saurav Kumar <124197263+Saurav13kr@users.noreply.github.com>
2025-05-10 10:01:26 +0530

    Update 0006-zigzag-conversion.cpp

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -6,8 +6,7 @@ public:
     vector<string>v(numRows, ""); 
 
     int j = 0, dir = -1;
-    for(int i = 0; i < s.length(); i++) 
-    {
+    for(int i = 0; i < s.length(); i++) {
         if(j == numRows - 1 || j == 0) dir *= (-1);  
         v[j] += s[i];
         if(dir == 1) j++;
